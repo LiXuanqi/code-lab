0217e5c06ca14725058bbd78172e27847fbaddd9
lixuanqi <lixuanqi1995@gmail.com>
2018-07-13 21:58:32 +0800

    Strings.removeExtraSpaces()

--- java/src/main/java/algorithm/Strings.java
+++ java/src/main/java/algorithm/Strings.java
@@ -1,6 +1,11 @@
 package algorithm;
 
 public class Strings {
+    /*
+    * @Desc: remove some particular chars from a string.
+    * @Input: "student", 'u'
+    * @Output: "stdent"
+    * */
     public static String removeChars(String str, char deleteChar) {
         if (str == null) {
             return "";
@@ -16,6 +21,34 @@ public class Strings {
         }
         String result = new String(input, 0, slow); // char[] => String
         System.out.println("Input: " + str + " delete: " + deleteChar);
+        System.out.println("Output: " + result);
+        return result;
+    }
+    /*
+    * @Desc: Remove all leading/trailing and duplicate empty spaces(only leave one empty space
+    *   if duplicated spaces happen) from input string.
+    * @Input:  "_ _ _ abc _ _ ed _ _ ef _ _"
+    * @Output: "abc_ed_ef"
+    * */
+    public static String removeExtraSpaces(String str) {
+        char[] input = str.toCharArray();
+        int slow = 0;
+        int fast = 0;
+        int wordCount = 0;
+        while (fast < input.length) {
+            while (fast < input.length && input[fast] == ' ') {
+                fast++;
+            }
+            if (fast < input.length && wordCount > 0) {
+                input[slow++] = ' ';
+            }
+            while (fast < input.length && input[fast] != ' ') {
+                input[slow++] = input[fast++];
+            }
+            wordCount++;
+        }
+        String result = new String(input, 0, slow);
+        System.out.println("Input: " + str);
         System.out.println("Output: " + result);
         return result;
     }
--- java/src/test/java/StringsTests.java
+++ java/src/test/java/StringsTests.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static algorithm.Strings.removeChars;
+import static algorithm.Strings.removeExtraSpaces;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringsTests {
@@ -9,5 +10,9 @@ class StringsTests {
     @DisplayName("remove particular char from a string")
     void removeCharsTest() {
         assertEquals("stdent", removeChars("studuuenuut", 'u'));
+    }
+    @Test
+    void removeExtraSpacesTest() {
+        assertEquals("abc ed ef", removeExtraSpaces("   abc  ed   ef  "));
     }
 }
