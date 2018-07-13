a21c9ca678581f607dbe60602483800508465b42
lixuanqi <lixuanqi1995@gmail.com>
2018-07-13 22:12:39 +0800

    format comment.

--- java/src/main/java/algorithm/Strings.java
+++ java/src/main/java/algorithm/Strings.java
@@ -1,11 +1,13 @@
 package algorithm;
 
 public class Strings {
-    /*
-    * @Desc: remove some particular chars from a string.
-    * @Input: "student", 'u'
-    * @Output: "stdent"
-    * */
+    /**
+     * Remove some particular chars from a string.
+     *
+     * @param str "student"
+     * @param deleteChar 'u'
+     * @return  "stdent"
+     */
     public static String removeChars(String str, char deleteChar) {
         if (str == null) {
             return "";
@@ -24,12 +26,14 @@ public class Strings {
         System.out.println("Output: " + result);
         return result;
     }
-    /*
-    * @Desc: Remove all leading/trailing and duplicate empty spaces(only leave one empty space
-    *   if duplicated spaces happen) from input string.
-    * @Input:  "_ _ _ abc _ _ ed _ _ ef _ _"
-    * @Output: "abc_ed_ef"
-    * */
+
+    /**
+     * Remove all leading/trailing and duplicate empty spaces(only leave one empty space if duplicated
+     * spaces happen) from input string.
+     *
+     * @param str "_ _ _ abc _ _ ed _ _ ef _ _"
+     * @return "abc_ed_ef"
+     */
     public static String removeExtraSpaces(String str) {
         char[] input = str.toCharArray();
         int slow = 0;
